9dbb5b888d59a26769ab6c23c37de5b1e843b911
wxyyxc1992 <384924552@qq.com>
2017-10-22 23:37:12 +0800

    更新内容

--- OpenSource/nest-boilerplate/src/modules/sync/controller/GithubSyncController.ts
+++ OpenSource/nest-boilerplate/src/modules/sync/controller/GithubSyncController.ts
@@ -1,11 +1,21 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { successResponse } from '../../../application/common/message/ResponseMessage';
 
-@Controller('/github')
-export class HelloController {
+@Controller('github')
+export class GithubSyncController {
   @Post()
-  sync() {
+  sync(@Req() request, @Body() body) {
     // 获取到请求数据
+    console.log(request.params);
 
     return 'Nest.js Boilerplate @ 王下邀月熊';
+  }
+
+  @Post('aw/:fileName')
+  awSync(@Req() request, @Body() body) {
+    // 获取到请求数据
+    console.log(request.params);
+
+    return successResponse;
   }
 }
